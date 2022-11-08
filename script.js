080ed23a9c9885b2fa3e8780a53aa8de2f22b02c
James Harrington <jamesharrington585@hotmail.com>
2022-11-08 01:15:15 -0500

    added character arrays

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment code here
 
+var lowArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~" ,"`", "{", "}", "[", "]", "|", ":", ";", "<", ",", ">", ".", "?", "/"];
+
 function generatePassword() {
   var passLength = prompt("How long would you like your password to be? Choose between 8 and 128 characters.")
 
