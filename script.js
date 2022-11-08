c38523007a335f05396f292da4a8506af1ff9f1c
James Harrington <jamesharrington585@hotmail.com>
2022-11-08 01:02:59 -0500

    created starter variables for question prompts

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Assignment code here
 
+function generatePassword() {
+  var passLength = prompt("How long would you like your password to be? Choose between 8 and 128 characters.")
+
+  var upperCase = confirm("Would you like uppercase letters?")
+
+  var lowerCase = confirm("Would you like lowercase letters?")
+
+  var numbers = confirm("Would you like numbers?")
+
+  var special = confirm("Would you like special characters?")
+
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
