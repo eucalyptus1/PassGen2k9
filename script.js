d76b73a669f26cfd8b60d390ed7d1e047542893d
James Harrington <jamesharrington585@hotmail.com>
2022-11-10 01:49:34 -0500

    added conditionals and array for chosen characters

--- script.js
+++ script.js
@@ -5,17 +5,35 @@ var upArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 var numbArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~" ,"`", "{", "}", "[", "]", "|", ":", ";", "<", ",", ">", ".", "?", "/"];
 
+var chosenArr = [];
+
 function generatePassword() {
   var passLength = prompt("How long would you like your password to be? Choose between 8 and 128 characters.")
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    alert("You must enter a number between 8 and 128")
+  }
 
   var upperCase = confirm("Would you like uppercase letters?")
+  if (upperCase) {
+    chosenArr.push(upArr);
+  }
 
   var lowerCase = confirm("Would you like lowercase letters?")
+  if (lowerCase) {
+    chosenArr.push(lowArr);
+  }
 
   var numbers = confirm("Would you like numbers?")
+  if (numbers) {
+    chosenArr.push(numbArr);
+  }
 
   var special = confirm("Would you like special characters?")
+  if (special) {
+    chosenArr.push(specialArr);
+  }
 
+  console.log(chosenArr);
 }
 
 
