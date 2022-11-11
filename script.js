707b658c53474a498342b1d75acb782b8818076e
James Harrington <jamesharrington585@hotmail.com>
2022-11-11 01:53:00 -0500

    fixed alerts not appearing and conditional for password length

--- script.js
+++ script.js
@@ -10,39 +10,38 @@ var chosenArr = [];
 function generatePassword() {
   var passLength = prompt("How long would you like your password to be? Choose between 8 and 128 characters.")
   if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-    alert("You must enter a number between 8 and 128")
+    alert("You must enter a number between 8 and 128");
+    return null;
   }
 
   var upperCase = confirm("Would you like uppercase letters?")
   if (upperCase) {
-    chosenArr = upArr.concat(chosenArr);
-    alert("Uppercase letters will be included.")
+    chosenArr = upArr.concat(chosenArr)
+    alert("Uppercase letters will be included.");
   }
 
   var lowerCase = confirm("Would you like lowercase letters?")
   if (lowerCase) {
-    chosenArr = lowArr.concat(chosenArr);
-    alert("Ok! Lowercase. Good choice.")
+    chosenArr = lowArr.concat(chosenArr)
+    alert("Ok! Lowercase. Good choice.");
   }
 
   var numbers = confirm("Would you like numbers?")
   if (numbers) {
-    chosenArr = numbArr.concat(chosenArr);
-    alert("There's going to be numbers in there too.")
+    chosenArr = numbArr.concat(chosenArr)
+    alert("There's going to be numbers in there too.");
   }
 
   var special = confirm("Would you like special characters?")
   if (special) {
-    chosenArr = specialArr.concat(chosenArr);
-    alert("This password is going to be extra special now.")
+    chosenArr = specialArr.concat(chosenArr)
+    alert("This password is going to be extra special now.");
   }
 
   if (!lowerCase && !upperCase && !number && !special) {
     alert("Your password can't be made out of air. You have to choose something!")
     return null;
   }
-
-  console.log(chosenArr);
 
   var finalPassword = "";
 
